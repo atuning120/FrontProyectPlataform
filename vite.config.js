1a0b887610b6705546cb63a73900f467abb718fb
atuning120 <90119017+atuning120@users.noreply.github.com>
2025-07-03 00:10:20 -0400

    naaaa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base:'',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '' : '/',
   plugins: [react()],
   server: {
-    host: true,   
-    port: 5173    
+    host: true,
+    port: 5173
   }
-})
+}))
